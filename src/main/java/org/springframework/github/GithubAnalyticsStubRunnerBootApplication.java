538980a01ea0c254c7bcb1e41964ea44a531176a
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-06-08 12:50:55 +0200

    first commit

---
+++ src/main/java/org/springframework/github/GithubAnalyticsStubRunnerBootApplication.java
@@ -0,0 +1,22 @@
+package org.springframework.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.test.binder.TestSupportBinderAutoConfiguration;
+
+@SpringBootApplication(exclude = TestSupportBinderAutoConfiguration.class)
+@EnableStubRunnerServer
+@EnableBinding
+public class GithubAnalyticsStubRunnerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAnalyticsStubRunnerBootApplication.class, args);
+	}
+
+	@AutoConfigureStubRunner
+	static class Config {}
+}
+
